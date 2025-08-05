909f7fd1016180eef31933ae6d228c8bb2902507
KenAndrew7 <kenandrew.francisco@cvsu.edu.ph>
2025-08-05 11:24:16 +0800

    Delete server directory

--- server/routes/projectRoutes.js
+++
@@ -1,8 +0,0 @@
-// server/routes/projectRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getAllProjects } = require('../controllers/projectController');
-
-router.get('/', getAllProjects);
-
-module.exports = router;
--- server/routes/resourceRoutes.js
+++
@@ -1,8 +0,0 @@
-// server/routes/resourceRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getAllResources } = require('../controllers/resourceController');
-
-router.get('/', getAllResources);
-
-module.exports = router;
